bae232da09cd85b749d0eac7fbde3ce5c88c46f3
Damola Oladipo <thedamolaoladipo@gmail.com>
2026-09-10 23:36:54 +0100

    feat: add HeroImage component
    Add the HeroImage component to this app.

---
+++ components/HeroImage.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import ThemedImage from '@/components/containers/ThemedImage';
+
+export default function HeroImage() {
+    return (
+        <section aria-label="Hero Image of the website" className="flow-root">
+            <div className="">
+                <ThemedImage
+                    src="/images/hero-listener.png"
+                    alt="A preview of troott"
+                    width={2400}
+                    height={1600}
+                    className="rounded-xl "
+                />
+            </div>
+        </section>
+    );
+}

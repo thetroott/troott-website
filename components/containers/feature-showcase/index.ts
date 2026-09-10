379a4ee623200bc3cf0866da874b54efbd8acfc7
Damola Oladipo <thedamolaoladipo@gmail.com>
2026-09-10 23:36:58 +0100

    feat: add index
    Add index to this codebase.

---
+++ components/containers/feature-showcase/index.ts
@@ -0,0 +1,8 @@
+export { CoreFeaturesSection } from './CoreFeaturesSection';
+export { FeatureShowcaseCard } from './FeatureShowcaseCard';
+export type {
+    CoreFeaturesSectionProps,
+    FeatureShowcaseCta,
+    FeatureShowcaseImage,
+    FeatureShowcaseItem,
+} from './types';

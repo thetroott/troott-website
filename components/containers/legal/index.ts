8276f50cee28b86078a905b6665402407bc6d025
Damola Oladipo <thedamolaoladipo@gmail.com>
2026-09-10 23:36:59 +0100

    feat: add index
    Add index to this codebase.

---
+++ components/containers/legal/index.ts
@@ -0,0 +1,4 @@
+export { LegalAudiencePage } from './LegalAudiencePage';
+export { LegalBreadcrumbs } from './LegalBreadcrumbs';
+export { LegalDocumentPage } from './LegalDocumentPage';
+export { LegalHubPage } from './LegalHubPage';

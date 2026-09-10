74eb6fc1c932a17a987d49c9e115c6b6e19750f8
Damola Oladipo <thedamolaoladipo@gmail.com>
2026-09-10 23:37:03 +0100

    feat: add index
    Add index to this codebase.

---
+++ components/shared/scrollspy/index.ts
@@ -0,0 +1,8 @@
+export { ScrollspyLayout } from './ScrollspyLayout';
+export { ScrollspyNavRow } from './ScrollspyNavRow';
+export {
+    getActivationLinePx,
+    getSiteHeaderScrollOffsetPx,
+    sectionDomId,
+} from './scrollspy-dom';
+export { useScrollspy } from './useScrollspy';

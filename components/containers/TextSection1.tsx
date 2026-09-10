8627f782b4c016f686569486ac369c7bdeff31fa
Damola Oladipo <thedamolaoladipo@gmail.com>
2026-09-10 23:36:56 +0100

    feat: add TextSection1 component
    Add the TextSection1 component to this app.

---
+++ components/containers/TextSection1.tsx
@@ -0,0 +1,16 @@
+import { TextReveal } from '../magicui/text-reveal';
+
+const PreText = () => {
+    return (
+        <section id="listener" className="relative min-h-[65vh]">
+            <TextReveal>
+                You want to grow spiritually. But first, you search once, twice
+                and countless time to find the sermon you wish to hear? Not
+                anymore. Troott puts every sermon you love old and new in your
+                pocket. Ad-free. Organised. Ready when you are.
+            </TextReveal>
+        </section>
+    );
+};
+
+export default PreText;

a5367d6127fe46e7517d7d22b7b1adcb85acd41d
Damola Oladipo <thedamolaoladipo@gmail.com>
2026-09-10 23:36:52 +0100

    feat: add section intro
    Add section intro to this codebase.

---
+++ _data/troott/section-intro.ts
@@ -0,0 +1,13 @@
+export type SectionIntroContent = {
+    id?: string;
+    label: string;
+    heading: string;
+    headingMuted: string;
+};
+
+export const whyTroottIntroContent: SectionIntroContent = {
+    id: 'why-troott-intro',
+    label: '// Built for real life',
+    heading: 'Find it. Listen.',
+    headingMuted: 'Share it.',
+};
